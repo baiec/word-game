e612063db9ebcfcb38311f82102ca2ea60dbfda8
baiec <alex.iosivas@gmail.com>
2018-03-06 19:44:43 -1000

    New start

--- script.js
+++ script.js
@@ -8,148 +8,79 @@ var pubnub = PUBNUB.init({
 	heartbeat: 10
 });
 
+/*
+    Runs on startup, displays start modal
+    n/a
+    void
+*/
+(function(){
+    "use strict";
+    $("#startModal").modal({keyboard:false,backdrop:"static"}); //removes ability to close modal
+    $("#startModal").modal("show");
+}());
 
-var noOfWordsInRow = 0;
+
 //arrays
 var consonantArray = ["b","c","d","f","g","h","j","k","l","m","n","p","r","s","t","v","w","y"];
 var vowelArray = ["a","e","i","o","u"];
 var submittedWordsArray = [];
 
 //containers
-//var lettersDiv = $("div#letterDisplay");
 var wordAreaDiv = $("div#wordArea");
-//var submitDiv = $("div#submitDiv");
 var startDiv = $("div#start");
 
-//var checkDiv = $("div#checkDiv");
-
-var consonantUl = $("ul#consonantUl");
-var vowelUl = $("ul#vowelUl");
-var notificationUl = $("#notificationList");
+var letterRow = $("#letterRow");
+var chatUl = $("#chatUl");
 
 var confirmWord = $("p#word");
 var confirmBtn = $("#confirmBtn");
 var error = $("#error");
-var noOfPlayers = $("p#noOfPlayers");
+var noOfPlayers = $("#noOfPlayers");
 //buttons
 var enterLobby = $("button#enterLobby");
 var startGame = $("button#startGame");
 var endGame = $("button#endGame");
+var chatBtn = $("#publishMessageBtn");
 //input
-
+var chatInput = $("#chatInput");
 //other
 var inLobby = true; //local state
 var scores = []; //will contain arrays which have [name, letters]
 var words = []; //user's words after clicking endgame, used to display them
 
-//pubnub portion of game
+
+/*
+    publishes message from chatInput to testChannel3
+    n/a
+    void
+*/
+function pubChatMsg(){
+    "use strict";
+    if(chatInput.val().trim() !== ''){ //checks if blank or only containing whitespace
+        pub("testChannel5",$("#usernameInput").val() + ": " +chatInput.val());
+        chatInput.val(''); 
+    }
+}
 
 
 /*
-function startGameCountdown() {
+    test function to see the elements in an array
+    array = whatever array you want to inspect
+    n/a
+*/
+function logArrayContents(array){
 	"use strict";
-	var minutes = 3, secs = 60;
-	var timer = setInterval(function(){
-		$("p#timerDisplay").html(minutes + ": " + secs);
-		if(secs === 0){
-			if(minutes === 0){
-				clearInterval(timer);	
-			}
-			else {
-				minutes--;
-				secs = 60;
-			}
-		}
-		else {
-			secs--;
-		}
-	},1000);
+	for(var i=0;i<array.length;i++){
+		console.log(array[i]);
+	}
 }
 
+
+/*
+	Will count how many letters you had
+	global parameter "words"
+	returns integer
 */
-
-
-
-
-
-
-$(window).load(function(){
-	"use strict";
-	notificationUl.hide();
-});
-
-
-
-
-
-
-
-
-//game and chat stuff below
-
-startGame.click(function(){ //will make all players start game
-	"use strict";
-	var vowels = generateRandomLetters(vowelArray, 3);
-	var consonants = generateRandomLetters(consonantArray, 4);
-	inLobby = false;
-	//start timer
-	pubnub.publish({
-		channel: "letters",
-		message: [vowels, consonants]
-	});
-	pubnub.state({
-		channel: "testChannel14",
-		state: {
-			inGame: true
-		},
-		callback: function(m){console.log(m.inGame);}
-	});
-});
-
-endGame.click(function(){ //will make all players end game
-	"use strict";
-	confirmWord.html('');
-	inLobby = true;
-	pubnub.state({
-		channel: "testChannel14",
-		state: {
-			inGame: false
-		},
-		callback: function(m){console.log(m.inGame);}
-	});
-	pub("endGameChannel",[$("#usernameInput").val(),getLetterAmount(),words]); //[username,integer,[words]]
-});
-
-enterLobby.click(function(){
-	"use strict";
-	//testChannel14
-	startDiv.css("display", "none");
-});
-
-/*function handleWordArray(m){ //m is an array, called when an array gets pub'd to channel "words" but only to whoever clicked the endgame button
-	"use strict";
-	alert(m[0]);
-	wordAreaDiv.prepend("<ul class='wordList'></ul>");
-	cancelOutWords(m[0],m[1]);	 //will just compare 2 arrays, not all of them.
-	for(var i=0;i<m.length;i++){
-		$("ul:first").append("<li>"+m[i]+"</li>");
-	}
-}*/
-
-/*function cancelOutWords(array1, array2){
-	"use strict";
-	var array = getLargestArray(array1, array2); //array[0] > array[1]
-	for(var i=0;i<array[0].length;i++){
-		for(var e=0;e<array[1].length;e++){
-			if(array[0][i] === array[1][e]){
-				array.splice([0][i],1);
-				array.splice([1][e],1);
-			}
-		}
-	}
-}*/
-
-
 function getLetterAmount(){
 	"use strict";
 	var letters = 0;
@@ -161,21 +92,28 @@ function getLetterAmount(){
 	return letters;
 }
 
-function decideWinner(m){
+
+/*
+	Will calculate winner (who has the most letters)
+	parameter m = player
+	void
+*/
+function decideWinner(player){
 	"use strict";
-	scores.push(m);
-	//listWords(m);
+	scores.push(player);
+    listWords(player);
 	pubnub.here_now({
-		channel: "testChannel14",
+		channel: "testChannel5",
 		callback: function(m){
 			if(scores.length === m.occupancy){
+                var letters = [];
+                var winners = [];
+                var largest = 0;
 				logArrayContents(scores);
-				var letters = [];
 				for(var i=0;i<scores.length;i++){
 					letters.push(scores[i][1]);
 				}
-				var largest = Math.max.apply(Math, letters);
-				var winners = [];
+				largest = Math.max.apply(Math, letters);
 				for(var e=0;e<scores.length;e++){
 					if(largest === scores[e][1]){
 						winners.push(scores[e][0] + " (" + scores[e][1] + ")");
@@ -188,67 +126,89 @@ function decideWinner(m){
 	});
 }
 
+function listWords(player){ // player[0] = username, player[1] = number of letters, player[2] = array of words
+    "use strict";
+    console.log("test");
+    $("#wordArea").append("<div class='col-md-2 col-sm-2 end'><ul><li>"+player[0]+" ("+player[1]+"): </li></ul></div>");
+    player[2].forEach(function(ele){
+        $("#wordArea div:last ul").append("<li>"+ele+"</li>");
+    });
+}
+
+/*
+	ran when startgame is clicked, emptys previous letters and displays new ones
+	m = the player
+	void
+*/
 function handleLetters(m){ // [vowels, consonants] eaCH ITEM IS An array
 	"use strict";
-	vowelUl.empty(); //i empty them here and not when ending the game so that people have the chance to see what other words they could make
-	consonantUl.empty();
-	for(var i=0 ; i<3 ; i++){ // 3 = m[0].vowels.length
-		vowelUl.append("<li><button>"+m[0][i]+"</button></li>");
+	letterRow.empty(); //i empty it here and not when ending the game so that people have the chance to see what other words they could make
+	for(var i=0 ; i<4 ; i++){
+        if(i === 0){
+		    letterRow.append("<div class='col-md-1 col-md-offset-2 col-sm-1 col-sm-offset-2 centerAlignContents'><button class='btn btn-ltr btn-warning btn-lg'>"+m[1][i]+"</button></div>");
+        } else {
+            letterRow.append("<div class='col-md-1 col-sm-1 centerAlignContents'><button class='btn btn-ltr btn-warning btn-lg'>"+m[1][i]+"</button></div>");
+        }
+        if(i !== 3){
+            letterRow.append("<div class='col-md-1 col-sm-1 centerAlignContents'><button class='btn btn-ltr btn-danger btn-lg'>"+m[0][i]+"</button></div>");
+        }
 	}
-	for(var e=0 ; e<4 ; e++){ // 4 = m[0].consonants.length
-		consonantUl.append("<li><button>"+m[1][e]+"</button></li>");
-	}
-} //called at start of game
+}
 
+
+/*
+	will collect words at end of round
+	n/a
+	returns array filled with words
+*/
 function putWordsIntoArray(){
 	"use strict";
-	var noOfTd = $("td").not(".special").length;
+    var word = "";
+	var noOfCol = $("#wordArea div").length;
 	var array = [];
-	for(var i=0;i<noOfTd;i++){
-		var word = $("td:not(.special):first");
-		array.push(word.text());
-		word.remove();
+	for(var i=0;i<noOfCol;i++){
+       word = $("#wordArea div:not(.end):first"); 
+       array.push(word.text());
+       word.remove();
 	}
 	return array;
-} 
+}
 
-/*function listWords(array){
-	"use strict";
-	$("tr:last").append("<ul class='listOfWords'></ul>");
-	for(var i=0;i<array[2].length;i++){
-		$("tr ul:last").append("<li>"+array[2][i]+"</li>");
-	}
-}*/
 
+/*
+	Handles all pubnub presence related events, such as joining and state-changing (starting/ending games)
+	m = the player
+	void
+*/
 function presence(m){
 	"use strict";
 	console.log(m);
 	if(m.action === "join"){
-		pub("testChannel14","Player joined");
 		if(uuid === m.uuid){ //only will run for the user who joined
 			pubnub.here_now({
-				channel: "testChannel14",
+				channel: "testChannel5",
 				state: true,
 				callback: function(m){
 					var p = m.uuids;
 					if(p[0].state.inGame && p[0].uuid !== uuid){
-						pubnub.unsubscribe({
-							channel: "testChannel14",
-							callback: alert("Game is in progress, you have been kicked")
-						});
+						throw new Error('This is not an error. This is just to abort javascript');
+						/*pubnub.unsubscribe({
+							channel: "testChannel5",
+							callback: alert("Game is in progress, you have been kicked, please refresh when game is finished")
+						});*/
 					}
 				}
 			});
 		}
 	}
 	else if(m.action === "timeout" || m.action === "leave"){
-		pub("testChannel14","Player left");	
 	}
 	else if(m.action === "state-change"){
 		if(m.data.inGame && inLobby){ //someone clicked the startgane button and you are not in the game
 			inLobby = false;
+            $("#wordArea").empty();
 			pubnub.state({
-				channel: "testChannel14",
+				channel: "testChannel5",
 				state: {
 					inGame: true
 				},
@@ -258,7 +218,7 @@ function presence(m){
 		else if(!m.data.inGame && !inLobby){
 			inLobby = true;
 			pubnub.state({
-				channel: "testChannel14",
+				channel: "testChannel5",
 				state: {
 					inGame: false
 				},
@@ -271,39 +231,22 @@ function presence(m){
 }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+/*
+    Prepends published messages to chatUl
+    message = what a player typed in the chatInput
+    n/a
+*/
 function handleMessage(message){
 	"use strict";
-	notificationUl.append("<li>"+message+"</li>");
+	chatUl.prepend("<li>"+message+"</li>");
 }
 
+
+/*
+	short notation to publish messages
+	channel = what channel to publish message to, message = what you want to send
+	void
+*/
 function pub(channel, message){
 	"use strict";
 	pubnub.publish({
@@ -312,6 +255,12 @@ function pub(channel, message){
 	});
 }
 
+
+/*
+	generates random consonants and vowels
+	array = consonant array or vowel array, noOfElementsNeeded = how many of each type of letter (vowels 3, consonants 4)
+	returns array with letters to be used in the game
+*/
 function generateRandomLetters(array,noOfElementsNeeded){ //noOfElementsNeeded = 3 or 4
 		"use strict";
 		var usedLetters = [];
@@ -327,16 +276,27 @@ function generateRandomLetters(array,noOfElementsNeeded){ //noOfElementsNeeded =
 		return usedLetters;
 }
 
+
+/*
+	checks and prevents the user from selecting two of the same letter in a word
+	letter =  the letter the user clicked
+	returns boolean
+*/
 function duplicateLetter(letter){
 	"use strict";
 	if((confirmWord.text()).indexOf(letter) > -1){
 		return true;	
-	}
-	else {
+    } else {
 		return false;
 	}
 }
 
+
+/*
+	checks and prevents the user from submitting a word below 3 letters
+	n/a
+	returns boolean
+*/
 function wordLengthAboveTwo(){
 	"use strict";
 	if((confirmWord.text()).length < 3){
@@ -346,6 +306,12 @@ function wordLengthAboveTwo(){
 	}
 }
 
+
+/*
+	checks and prevents the user from submitting a word they have already sumbitted
+	n/a
+	returns boolean
+*/
 function repeatWords(){
 	"use strict";
 	if(submittedWordsArray.indexOf(confirmWord.text()) === -1){
@@ -355,7 +321,112 @@ function repeatWords(){
 	}
 }
 
-$(document).on('click', 'ul li button', function(){
+
+function realWord(){
+    "use strict";
+    if(dict.indexOf(confirmWord.text()) !== -1){
+        return true;
+    } else {
+        return false;
+    }
+}
+
+// event handlers
+
+/*
+    If join button is clicked and username isnt blank, hide modal
+    n/a
+    void
+*/
+enterLobby.click(function(){
+    "use strict";
+    if($("#usernameInput").val().trim() !== ""){
+        $("#startModal").modal("hide");
+    } else {
+        $("#startModalError").text("Username is blank.");
+    }
+});
+
+
+/*
+    If enter key is clicked and username isnt blank, hide modal
+    n/a
+    void
+*/
+$("#usernameInput").keypress(function(key){
+   "use strict";
+   if(key.which === 13){
+       enterLobby.click();   
+   }
+});
+
+
+/* 
+	Will generate letters and change state to ingame
+	No parameters
+	Void
+*/
+startGame.click(function(){
+	"use strict";
+	var vowels = generateRandomLetters(vowelArray, 3);
+	var consonants = generateRandomLetters(consonantArray, 4);
+    $("#wordArea").empty();
+	inLobby = false;
+	//start timer
+	pub("letters",[vowels, consonants]);
+	pubnub.state({
+		channel: "testChannel5",
+		state: {
+			inGame: true
+		},
+		callback: function(m){console.log(m.inGame);}
+	});
+});
+
+
+/*
+	Will set ingame state to false and submit score
+	N/A
+	Void
+*/
+
+endGame.click(function(){ //will make all players end game
+	"use strict";
+	confirmWord.html('');
+    
+	inLobby = true;
+	pubnub.state({
+		channel: "testChannel5",
+		state: {
+			inGame: false
+		},
+		callback: function(m){console.log(m.inGame);}
+	});
+	pub("endGameChannel",[$("#usernameInput").val(),getLetterAmount(),words]); //[username,integer,[words]]
+    submittedWordsArray.length = 0;
+});
+
+
+/*
+	Will hide the username div
+	n/a
+	void
+*/
+
+enterLobby.click(function(){
+	"use strict";
+	//testChannel3
+	startDiv.css("display", "none");
+});
+
+
+/*
+	handles all the cases when the user clicks the letter buttons; letters clicked will be added to current word
+	n/a
+	void
+*/
+
+$(document).on('click', '#letterRow div button', function(){
 	"use strict";
 	if(inLobby){
 		error.text("The buttons don't work.");
@@ -371,21 +442,24 @@ $(document).on('click', 'ul li button', function(){
 	}
 });
 
+
+/*
+	handles all cases when user clicks the submit word button; appends current word to table
+	n/a
+	void
+*/
 confirmBtn.click(function(){
 	"use strict";
 	if(wordLengthAboveTwo()){
 		if(!repeatWords()){
-			if(noOfWordsInRow === 5){
-				$("#tableOfSubmittedWords").append("<tr><td><p>"+confirmWord.text()+"</p></td></tr>");
-				noOfWordsInRow++;
-				noOfWordsInRow = 1;
-			} else {
-				$("#tableOfSubmittedWords tr:last").append("<td><p>"+confirmWord.text()+"</p></td>");
-				noOfWordsInRow++;
-			}
-			submittedWordsArray.push(confirmWord.text());
-			confirmWord.text('');
-			error.text('');
+            if(realWord()){
+                $("#wordArea").append('<div class="col-md-2 col-sm-2 wordAreaCol">'+confirmWord.text()+'</div>');
+                submittedWordsArray.push(confirmWord.text());
+                confirmWord.text('');
+                error.text('');
+            } else {
+                error.text("That isn't an English word.");
+            }
 		} else {
 			error.text("You cannot submit two of the same word.");
 		}
@@ -394,43 +468,60 @@ confirmBtn.click(function(){
 	}
 });
 
+
+/*
+	when current word is clicked, it is deleted
+	n/a
+	void
+*/
 $(document).on('click', 'p#word', function(){
 	"use strict";
 	confirmWord.text('');
 });
 
+
+/*
+    publishes message from chatInput when enter key is pressed
+    e = key pressed
+    void
+*/
+chatInput.keypress(function(e){
+    "use strict";
+    if(e.which === 13){ //13 === enter
+        pubChatMsg();
+    }
+});
+
+
+/*
+    publishes message from chatInput when post button is clicked
+    n/a
+    void
+*/
+chatBtn.click(function(){
+    "use strict";
+    pubChatMsg();
+});
+
 pubnub.subscribe({
-	channel: "testChannel14",
+	channel: "testChannel5",
 	presence: presence,
 	message: handleMessage,
 	state: {
 		inGame:false
 	}
-}); //"normal" (presence and if there was chat)
+}); //"normal" (presence and chat)
+
 
 pubnub.subscribe({
 	channel: "letters",
 	message: handleLetters
 }); //for handling start of game
 
+
 pubnub.subscribe({
 	channel: "endGameChannel",
 	message: decideWinner
 });
 
-function logArrayContents(array){ //for testing
-	"use strict";
-	for(var i=0;i<array.length;i++){
-		console.log(array[i]);
-	}
-}
 
-/*function getLargestArray(array1, array2){ //returns array with 1 element being larger
-	"use strict";
-	if(array1.length > array2.length){
-		return [array1,array2];
-	}
-	else{
-		return [array2,array1]; //this will work if they are the same length
-	}
-}*/
